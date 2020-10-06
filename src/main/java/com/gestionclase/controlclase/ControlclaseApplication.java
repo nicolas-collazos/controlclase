c665cdc006e06acac01af699a3c0abae973a5c62
nicolas-collazos <you@example.com>
2020-10-06 18:48:16 -0500

    first commit

---
+++ src/main/java/com/gestionclase/controlclase/ControlclaseApplication.java
@@ -0,0 +1,13 @@
+package com.gestionclase.controlclase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlclaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlclaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestionclase/controlclase/ControlclaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestionclase.controlclase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlclaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
